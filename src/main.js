c28a4127bd8f4ba7d419a6ad67d0d694a969197e
Abhay Manoj <melchior88729@gmail.com>
2026-01-12 20:36:25 -0500

    made deltatime calc into function for main

--- src/main.js
+++ src/main.js
@@ -16,11 +16,25 @@ const world = new World(sideLength);
 const player = new Player();
 let lastTime = 0;
 
-function main(currentTime) {
-  const deltaTime = (currentTime - lastTime) / 1000;
+/**
+* Returns the time since the last 'tick'.
+* @param {number} currentTime - The current time
+* @returns {number} The difference in time from the last time
+*/
+function getDeltaTime(currentTime) {
+  const msPerSecond = 1000;
+  const deltaTime = (currentTime - lastTime) / msPerSecond;
   lastTime = currentTime;
-  const fixedDeltaTime = Math.min(deltaTime, 0.1);
+  return Math.min(deltaTime, 0.1);
+}
 
+/**
+* Calculates the difference in time, moves the player, and draws to the screen.
+* Ran through 'requestAnimationFrame'.
+* @param {number} currentTime - This is handled by requestAnimationFrame.
+*/
+function main(currentTime) {
+  const deltaTime = getDeltaTime(currentTime);
   controller.movePlayer(player, fixedDeltaTime);
   artist.drawScreen(world);
   artist.drawPlayer(player);
