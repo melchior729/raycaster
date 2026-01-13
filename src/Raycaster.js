fdf46e1e8a0953a929532a9a803857eb8e094dd8
Abhay Manoj <melchior88729@gmail.com>
2026-01-13 02:12:25 -0500

    wrote explanation of the ray lenghts function

--- src/Raycaster.js
+++ src/Raycaster.js
@@ -3,7 +3,29 @@ export default class Raycaster {
   constructor(width) {
     this.width = width;
     this.rayLengths = [];
-    this.fov = { x: 0, y: 1 };
-    this.camera = { x: 1, y: 0 }
+  }
+
+  getRays(player) {
+    // loop from 0 to the widht of the screen
+    // make a function that scales it properly (-1 -> 1)
+    // get the player direction vector
+    // get the normal of that vector;
+    // scale it by the constant 
+    // add it to the player dir vector.
+    // add the vector to the pos of the player.
+    // shoot the ray and do dda, get back the length
+    // store this in rayLengths[i]
+
+    for (let i = 0; i < width; i++) {
+
+    }
+  }
+
+  dda(direction, grid) {
+    // it will return sidedist x or siddedist 13/01/2026
+  }
+
+  clearRays() {
+    this.rayLengths = [];
   }
 }
